623d5fc9268b19a740c8d4ec0d8f7a180e9b607e
Joseph W Becher <jwbecher@drazisil.com>
2017-02-11 20:21:35 -0500

    No longer makes obsidial platform
    ...
    ...
    * There are mobs up there too...
    * Portal block doesn't stay, but comes back if you put grass in it

--- src/main/java/com/drazisil/creativedimensions/world/TeleporterCreative.java
+++ src/main/java/com/drazisil/creativedimensions/world/TeleporterCreative.java
@@ -81,7 +81,7 @@ public class TeleporterCreative extends Teleporter
             int l = 1;
             int i1 = 0;
 
-            for (int j1 = -2; j1 <= 2; ++j1)
+/*            for (int j1 = -2; j1 <= 2; ++j1)
             {
                 for (int k1 = -2; k1 <= 2; ++k1)
                 {
@@ -94,7 +94,7 @@ public class TeleporterCreative extends Teleporter
                         this.worldServerInstance.setBlockState(new BlockPos(i2, j2, k2), flag ? Blocks.OBSIDIAN.getDefaultState() : Blocks.AIR.getDefaultState());
                     }
                 }
-            }
+            }*/
 
             entityIn.setLocationAndAngles((double)i, (double)j, (double)k, entityIn.rotationYaw, 0.0F);
             entityIn.motionX = 0.0D;
@@ -480,7 +480,7 @@ public class TeleporterCreative extends Teleporter
             player.getServer().getPlayerList().updatePermissionLevel(player);
             worldserver.removeEntityDangerously(player);
             player.isDead = false;
-            player.getServer().getPlayerList().transferEntityToWorld(player, i, worldserver, worldserver1, teleporter);
+            transferEntityToWorld(player, i, worldserver, worldserver1, teleporter);
             player.getServer().getPlayerList().preparePlayer(player, worldserver);
 
             BlockPos safePos = player.getServer().getPlayerList().getServerInstance().worldServerForDimension(dimensionIn).getTopSolidOrLiquidBlock(new BlockPos(player.posX, player.posY, player.posZ));
@@ -496,6 +496,88 @@ public class TeleporterCreative extends Teleporter
                 player.connection.sendPacket(new SPacketEntityEffect(player.getEntityId(), potioneffect));
             }
             net.minecraftforge.fml.common.FMLCommonHandler.instance().firePlayerChangedDimensionEvent(player, i, dimensionIn);
+        }
+
+        public static void transferEntityToWorld(Entity entityIn, int lastDimension, WorldServer oldWorldIn, WorldServer toWorldIn, net.minecraft.world.Teleporter teleporter)
+        {
+            net.minecraft.world.WorldProvider pOld = oldWorldIn.provider;
+            net.minecraft.world.WorldProvider pNew = toWorldIn.provider;
+            double moveFactor = pOld.getMovementFactor() / pNew.getMovementFactor();
+            double d0 = entityIn.posX * moveFactor;
+            double d1 = entityIn.posZ * moveFactor;
+            double d2 = 8.0D;
+            float f = entityIn.rotationYaw;
+            oldWorldIn.theProfiler.startSection("moving");
+
+            if (false && entityIn.dimension == -1)
+            {
+                d0 = MathHelper.clamp_double(d0 / 8.0D, toWorldIn.getWorldBorder().minX() + 16.0D, toWorldIn.getWorldBorder().maxX() - 16.0D);
+                d1 = MathHelper.clamp_double(d1 / 8.0D, toWorldIn.getWorldBorder().minZ() + 16.0D, toWorldIn.getWorldBorder().maxZ() - 16.0D);
+                entityIn.setLocationAndAngles(d0, entityIn.posY, d1, entityIn.rotationYaw, entityIn.rotationPitch);
+
+                if (entityIn.isEntityAlive())
+                {
+                    oldWorldIn.updateEntityWithOptionalForce(entityIn, false);
+                }
+            }
+            else if (false && entityIn.dimension == 0)
+            {
+                d0 = MathHelper.clamp_double(d0 * 8.0D, toWorldIn.getWorldBorder().minX() + 16.0D, toWorldIn.getWorldBorder().maxX() - 16.0D);
+                d1 = MathHelper.clamp_double(d1 * 8.0D, toWorldIn.getWorldBorder().minZ() + 16.0D, toWorldIn.getWorldBorder().maxZ() - 16.0D);
+                entityIn.setLocationAndAngles(d0, entityIn.posY, d1, entityIn.rotationYaw, entityIn.rotationPitch);
+
+                if (entityIn.isEntityAlive())
+                {
+                    oldWorldIn.updateEntityWithOptionalForce(entityIn, false);
+                }
+            }
+
+            if (entityIn.dimension == 1)
+            {
+                BlockPos blockpos;
+
+                if (lastDimension == 1)
+                {
+                    blockpos = toWorldIn.getSpawnPoint();
+                }
+                else
+                {
+                    blockpos = toWorldIn.getSpawnCoordinate();
+                }
+
+                d0 = (double)blockpos.getX();
+                entityIn.posY = (double)blockpos.getY();
+                d1 = (double)blockpos.getZ();
+                entityIn.setLocationAndAngles(d0, entityIn.posY, d1, 90.0F, 0.0F);
+
+                if (entityIn.isEntityAlive())
+                {
+                    oldWorldIn.updateEntityWithOptionalForce(entityIn, false);
+                }
+            }
+
+            oldWorldIn.theProfiler.endSection();
+
+            if (lastDimension != 1)
+            {
+                oldWorldIn.theProfiler.startSection("placing");
+                d0 = (double)MathHelper.clamp_int((int)d0, -29999872, 29999872);
+                d1 = (double)MathHelper.clamp_int((int)d1, -29999872, 29999872);
+
+                if (entityIn.isEntityAlive())
+                {
+                    entityIn.setLocationAndAngles(d0, entityIn.posY, d1, entityIn.rotationYaw, entityIn.rotationPitch);
+
+                    teleporter.placeInPortal(entityIn, f);
+
+                    toWorldIn.spawnEntityInWorld(entityIn);
+                    toWorldIn.updateEntityWithOptionalForce(entityIn, false);
+                }
+
+                oldWorldIn.theProfiler.endSection();
+            }
+
+            entityIn.setWorld(toWorldIn);
         }
 
     }
